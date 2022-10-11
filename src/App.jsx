8df313ea292cc06214e494cb33d6a9df1a62455f
Maxime Crampon <maximec@hokla.com>
2022-10-11 11:17:10 +0200

    feat: filter patients on name

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,49 @@
-import { Table } from 'antd';
-import 'antd/dist/antd.css';
-import './App.css';
-import patients from './data/patients.json';
+import { Input, Table } from "antd";
+import "antd/dist/antd.css";
+import { useState } from "react";
+import "./App.css";
+import patients from "./data/patients.json";
 
 function App() {
-  const dataSource = patients;
+  const [dataSource, setDataSource] = useState(patients);
+
+  const filterPatients = (searchTerm) => {
+    const filteredPatients = patients.filter((patient) =>
+      patient.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setDataSource(filteredPatients);
+  };
+
   const columns = [
     {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-      width: '20%',
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      width: "20%",
     },
     {
-      title: 'E-mail',
-      dataIndex: 'email',
-      key: 'email',
-      width: '40%',
+      title: "E-mail",
+      dataIndex: "email",
+      key: "email",
+      width: "40%",
     },
     {
-      title: 'Birthdate',
-      dataIndex: 'birthdate',
-      key: 'birthdate',
-      width: '20%',
+      title: "Birthdate",
+      dataIndex: "birthdate",
+      key: "birthdate",
+      width: "20%",
     },
     {
-      title: 'General Practitioner',
-      dataIndex: 'generalPractitioner',
-      key: 'generalPractitioner',
-      width: '20%',
+      title: "General Practitioner",
+      dataIndex: "generalPractitioner",
+      key: "generalPractitioner",
+      width: "20%",
     },
   ];
 
   return (
     <div className="table-container">
+      <Input.Search onSearch={(searchTerm) => filterPatients(searchTerm)} />
       <Table dataSource={dataSource} columns={columns} />
     </div>
   );
